30adf2aa00469358c3ac2965d1615d02d7014191
Jing <boundino@gmail.com>
2018-10-12 12:00:49 -0400

    cms left/right

--- xjjlibs/xjjroot/xjjrootuti.h
+++ xjjlibs/xjjroot/xjjrootuti.h
@@ -17,9 +17,9 @@ namespace xjjroot
   const float margin_pad_bottom = 0.145;
   const float margin_pad_top = 0.1;
 
-  const Color_t colorlist_light[]  = {kGreen-8, kOrange-4, kRed-9, kAzure-9, kMagenta-8, kCyan-10, kViolet-9, kYellow-10, kBlue-8, kPink+1};
-  const Color_t colorlist_middle[] = {kGreen+2, kOrange-3, kRed-3, kAzure-3, kMagenta-5, kCyan-6,  kViolet+7, kYellow+2,  kBlue-5, kPink+2};
-  const Color_t colorlist_dark[]   = {kGreen+3, kOrange+5, kRed+2, kAzure-6, kMagenta-1, kCyan+3,  kViolet+4, kYellow+3,  kBlue-1, kPink+3};
+  const Color_t colorlist_light[]  = {kGreen-8, kOrange-4, kRed-9, kAzure-9, kMagenta-8, kCyan-10, kYellow-10, kBlue-8, kPink+1, kViolet-9};
+  const Color_t colorlist_middle[] = {kGreen+2, kOrange-3, kRed-3, kAzure-3, kMagenta-5, kCyan-6,  kYellow+2,  kBlue-5, kPink+2, kViolet+7};
+  const Color_t colorlist_dark[]   = {kGreen+3, kOrange+5, kRed+2, kAzure-6, kMagenta-1, kCyan+3,  kYellow+3,  kBlue-1, kPink+3, kViolet+4};
   const int ncolor = sizeof(colorlist_light)/sizeof(colorlist_light[0]);
 
   void setgstyle(Int_t padtick=0);
@@ -28,6 +28,8 @@ namespace xjjroot
   template <class T> void setthgrstyle(T* h, Color_t mcolor=-1, Style_t mstyle=-1, Size_t msize=-1, Color_t lcolor=-1, Style_t lstyle=-1, Width_t lwidth=-1, Color_t fcolor=-1, Float_t falpha=-1, Style_t fstyle=-1);
   template <class T> void setlinestyle(T* h, Color_t lcolor=-1, Style_t lstyle=-1, Width_t lwidth=-1);
   template <class T> void setmarkerstyle(T* h, Color_t mcolor=-1, Style_t mstyle=-1, Size_t msize=-1);
+  void drawCMSleft(TString content="#scale[1.25]{CMS} Preliminary", Float_t xpos=0, Float_t ypos=0);
+  void drawCMSright(TString content="", Float_t xpos=0, Float_t ypos=0);
   void drawCMS(TString collision="", TString snn="5.02", Float_t xpos=0, Float_t ypos=0, Bool_t drawenergy=true);
   void settex(TLatex* tex, Float_t tsize=0.04, Short_t align=12, Style_t font=42);
   void drawtex(Double_t x, Double_t y, const char *text, Float_t tsize=0.04, Short_t align=12, Style_t font=42);
@@ -144,6 +146,26 @@ void xjjroot::drawCMS(TString collision/*=""*/, TString snn/*="5.02"*/, Float_t 
   texCms->Draw();
   if(!drawenergy) return;
   TLatex* texCol = new TLatex(0.96+xpos,0.93+ypos, Form("%s #sqrt{s_{NN}} = %s TeV", collision.Data(), snn.Data()));
+  texCol->SetNDC();
+  texCol->SetTextAlign(32);
+  texCol->SetTextSize(0.04);
+  texCol->SetTextFont(42);
+  texCol->Draw();
+}
+
+void xjjroot::drawCMSleft(TString content/*=""*/, Float_t xpos/*=0*/, Float_t ypos/*=0*/)
+{
+  TLatex* texCms = new TLatex(0.18+xpos,0.93+ypos, content.Data());
+  texCms->SetNDC();
+  texCms->SetTextAlign(12);
+  texCms->SetTextSize(0.04);
+  texCms->SetTextFont(42);
+  texCms->Draw();
+}
+
+void xjjroot::drawCMSright(TString content/*=""*/, Float_t xpos/*=0*/, Float_t ypos/*=0*/)
+{
+  TLatex* texCol = new TLatex(0.96+xpos,0.92+ypos, content.Data());
   texCol->SetNDC();
   texCol->SetTextAlign(32);
   texCol->SetTextSize(0.04);
