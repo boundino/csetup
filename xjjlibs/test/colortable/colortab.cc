2aaddf24695efd61ece8e8e2de2cdc89f12c1dbe
Jing <boundino@gmail.com>
2019-08-05 17:18:27 -0400

    up colortab

--- xjjlibs/test/colortable/colortab.cc
+++ xjjlibs/test/colortable/colortab.cc
@@ -1,9 +1,12 @@
 #include <TCanvas.h>
 #include <vector>
 #include <iostream>
+#include <sstream>
 
 #include "xjjrootuti.h"
 
+std::vector<TString> colorlist_name = {"kGreen", "kOrange", "kRed", "kAzure", "kMagenta", "kCyan", "kYellow", "kBlue", "kPink", "kViolet"};
+std::vector<Color_t> colorlist_baseline = {kGreen, kOrange, kRed, kAzure, kMagenta, kCyan, kYellow, kBlue, kPink, kViolet};
 void colortab()
 {
   int ncol = 0, nrow = 0;
@@ -23,7 +26,7 @@ void colortab()
           xjjroot::drawbox(x1, y2, x2, y3, cc, 0.3, 1001, cc, 0, 0);
           xjjroot::drawline(x1, yl, x2, yl, cc, 2, 4);
           xjjroot::drawtex((x1+x2)/2., (y1+y2)/2., Form("%d", cc), 0.05, 22, 62, 0);
-          if(!ncol) xjjroot::drawtex(x1, y1, Form("%s", xjjroot::colorlist_name[nrow].Data()), 0.03, 13, 62, 0);
+          xjjroot::drawtex(x1, y1, Form("%s%s%d", colorlist_name[nrow].Data(), (cc>colorlist_baseline[nrow]?"+":"-"), abs(cc-colorlist_baseline[nrow])), 0.03, 13, 62, 0);
           nrow++;
         }
       ncol++;
@@ -43,7 +46,11 @@ void colortab()
           xjjroot::drawbox(x1, y1, x2, y2, cc.second, 1., 1001, cc.second, 0, 0);
           xjjroot::drawbox(x1, y2, x2, y3, cc.second, 0.3, 1001, cc.second, 0, 0);
           xjjroot::drawline(x1, yl, x2, yl, cc.second, 2, 4);
-          xjjroot::drawtex((x1+x2)/2., (y1+y2)/2., Form("%d", cc.second), 0.05, 22, 62, 0);
+          std::ostringstream oshex;
+          TColor::SaveColor(oshex, cc.second);
+          std::string strhex(oshex.str());
+          auto pos1 = strhex.find("#"), pos2 = strhex.find("\"", pos1+1);
+          xjjroot::drawtex((x1+x2)/2., (y1+y2)/2., std::string(strhex, pos1+1, pos2-pos1-1).c_str(), 0.05, 22, 62, 0);
           if(!ncol) xjjroot::drawtex(x1, y1, Form("%s", cc.first.c_str()), 0.03, 13, 62, 0);
           nrow++;
         }
