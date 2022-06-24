89e1b99f7171c1b365e35c0330a5783feaca5e1d
boundino <boundino@gmail.com>
2022-06-24 15:37:29 -0400

    add config vv

--- xjjlibs/xjjc/config.h
+++ xjjlibs/xjjc/config.h
@@ -14,9 +14,11 @@ namespace xjjc
   {
   public:
     config(std::string input) : input_(input) { parse(); }
+    bool goodkey(std::string key) { return value_.find(key) != value_.end(); }
     std::string v(std::string key) { return value_[key]; }
     float vf(std::string key) { return atof(value_[key].c_str()); }
     int vi(std::string key) { return atoi(value_[key].c_str()); }
+    std::vector<std::string> vv(std::string key) { return str_divide(value_[key], ","); }
     std::vector<float> vvf(std::string key);
     void print();
   private:
@@ -38,9 +40,9 @@ void xjjc::config::parse()
     }
 }
 
-std::vector<float> vvf(std::string key)
+std::vector<float> xjjc::config::vvf(std::string key)
 {
-  auto vv = xjjc::str_divide(key, ",");
+  std::vector<std::string> vv = str_divide(value_[key], ",");
   std::vector<float> v_result;
   for(auto& iv : vv)
     v_result.push_back(atof(iv.c_str()));
