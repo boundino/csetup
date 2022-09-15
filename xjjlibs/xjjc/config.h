b024b1a86bec207c51df9e651c7071cc100845b7
boundino <boundino@gmail.com>
2022-09-15 09:08:10 -0400

    add good key check for config

--- xjjlibs/xjjc/config.h
+++ xjjlibs/xjjc/config.h
@@ -15,10 +15,11 @@ namespace xjjc
   public:
     config(std::string input) : input_(input) { parse(); }
     bool goodkey(std::string key) { return value_.find(key) != value_.end(); }
-    std::string v(std::string key) { return value_[key]; }
-    float vf(std::string key) { return atof(value_[key].c_str()); }
-    int vi(std::string key) { return atoi(value_[key].c_str()); }
-    std::vector<std::string> vv(std::string key) { return str_divide(value_[key], ","); }
+    std::string v(std::string key) { return goodkey(key)?value_[key]:""; }
+    std::string operator[](std::string key) { return v(key); }
+    float vf(std::string key) { return atof(v(key).c_str()); }
+    int vi(std::string key) { return atoi(v(key).c_str()); }
+    std::vector<std::string> vv(std::string key) { return str_divide(v(key), ","); }
     std::vector<float> vvf(std::string key);
     void print();
   private:
@@ -34,7 +35,8 @@ void xjjc::config::parse()
   for(std::string line; std::getline(filein, line);)
     {
       if(!xjjc::str_contains(line, "=")) continue;
-      line = xjjc::str_eraseall(line, " ");
+      line = xjjc::str_replaceall(line, " =", "=");
+      line = xjjc::str_replaceall(line, "= ", "=");
       auto vline = xjjc::str_divide(line, "=");
       value_[vline[0]] = vline[1];
     }
@@ -42,7 +44,7 @@ void xjjc::config::parse()
 
 std::vector<float> xjjc::config::vvf(std::string key)
 {
-  std::vector<std::string> vv = str_divide(value_[key], ",");
+  std::vector<std::string> vv = str_divide(v(key), ",");
   std::vector<float> v_result;
   for(auto& iv : vv)
     v_result.push_back(atof(iv.c_str()));
