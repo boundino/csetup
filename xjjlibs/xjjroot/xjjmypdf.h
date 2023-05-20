50c798084a5408006681a3ac221e5fa668b725ea
boundino <boundino@gmail.com>
2023-05-20 18:28:08 +0200

    add option for mypdf to save png of each page

--- xjjlibs/xjjroot/xjjmypdf.h
+++ xjjlibs/xjjroot/xjjmypdf.h
@@ -4,13 +4,13 @@
 #include "xjjrootuti.h"
 
 /*
-xjjroot::mypdf pdf("output.pdf");
-// --> loop
-pdf.prepare();
-// draw
-pdf.write();
-// <--
-pdf.close();
+  xjjroot::mypdf pdf("output.pdf");
+  // --> loop
+  pdf.prepare();
+  // draw
+  pdf.write();
+  // <--
+  pdf.close();
 */
 namespace xjjroot
 {
@@ -20,7 +20,7 @@ namespace xjjroot
     mypdf(std::string filename, std::string canvasname, int ww, int hh);
     mypdf(TCanvas* cc, std::string filename);
     void prepare() { fc->Clear(); fc->cd(); }
-    void write() { fc->Print(Form("%s", ffname.c_str())); }
+    void write(std::string pngname = "", std::string opt = "W");
     void close() { fc->Print(Form("%s]", ffname.c_str())); delete fc; }
     TCanvas* getc() { return fc; }
     std::string getfilename() { return ffname; }
@@ -30,14 +30,23 @@ namespace xjjroot
   };
 }
 
-xjjroot::mypdf::mypdf(std::string filename, std::string canvasname, int ww, int hh) : ffname(filename)
-{
+xjjroot::mypdf::mypdf(std::string filename, std::string canvasname,
+                      int ww, int hh) : ffname(filename) {
   mkdir(ffname);
   fc = new TCanvas(canvasname.c_str(), "", ww, hh);
   fc->Print(Form("%s[", ffname.c_str()));
 }
 
-xjjroot::mypdf::mypdf(TCanvas* cc, std::string filename) : ffname(filename), fc(cc)
-{
+xjjroot::mypdf::mypdf(TCanvas* cc, std::string filename)
+  : ffname(filename), fc(cc) {
   fc->Print(Form("%s[", ffname.c_str()));
+}
+
+void xjjroot::mypdf::write(std::string pngname, std::string opt) {
+  if (pngname != "" && opt.find("W") != std::string::npos)
+    drawcomment(pngname);
+  fc->Print(Form("%s", ffname.c_str()));
+  if (pngname != "") {
+    fc->SaveAs(pngname.c_str());
+  }
 }
