6f84647d1543777807a9b635d22f80600c2aebe1
boundino <boundino@gmail.com>
2025-01-23 20:26:26 +0100

    add lsetup pub

---
+++ lpub/mypastel-blink.js
@@ -0,0 +1,26 @@
+black       = '#616161';
+red         = '#e06c75';
+green       = '#9bcc9b';
+yellow      = '#eacb96';
+blue        = '#99caec';
+magenta     = '#edb2c3';
+cyan        = '#92c5bf';
+white       = '#f1f1f1';
+lightBlack  = '#8e8e8e';
+lightRed    = '#f5c6bf';
+lightGreen  = '#ddfbc0';
+lightYellow = '#fefdd9';
+lightBlue   = '#c8e2fc';
+lightMagenta= '#f3b5f9';
+lightCyan   = '#b8fcf0';
+lightWhite  = '#fefefe';
+
+t.prefs_.set('color-palette-overrides',
+             [ black, red, green, yellow,
+               blue, magenta, cyan, white, 
+               lightBlack, lightRed, lightGreen, lightYellow,
+               lightBlue, lightMagenta, lightCyan, lightWhite ]); 
+
+t.prefs_.set('foreground-color', "#e5e5e5");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', 'rgba(243,183,125,0.5)');
